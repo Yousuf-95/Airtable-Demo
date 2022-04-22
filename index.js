298a82f63cc2df9bf9aeb3763185f4c4cf4c1495
Yousuf Suhail <yf.yousuf95@gmail.com>
2022-04-22 08:37:15 +0530

    Added update, create, delete routes

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 require('dotenv').config();
 const express = require('express');
 const Airtable = require('airtable');
+const bodyParser = require('body-parser');
+
+const apiKey = process.env.API_KEY;
+var base = new Airtable({apiKey: apiKey}).base('app1qPwyes1jpgpmf');
 
 const app = express();
 
-const apiKey = process.env.API_KEY;
 
-var base = new Airtable({apiKey: apiKey}).base('app1qPwyes1jpgpmf');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-app.get('/', (req,res) => {
-    const result = {};
 
+//Get records route
+
+app.get('/get-records', (req,res) => {
+    const result = [];
     base('Projects').select({
         view: 'Grid view'
     })
@@ -18,34 +24,83 @@ app.get('/', (req,res) => {
         if (err) { console.error(err); return; }
         else{
             records.forEach((record,id) =>  {
-                let name = record.get('Name');
-                result[id] = name;
+                console.log(record);
+                const myObject = Object.assign({}, {
+                    id: record.id,
+                    fields: record.fields
+                });
+                result.push(myObject);
             });
-
-            return res.json({message: result});
+            return res.send(result);
         }
     });
-})
+});
 
-//Update records code sample
+//Update records route
 
-// base('Projects').update([
-//     {
-//       "id": "recAECwr2OCqEbLVb",
-//       "fields": {
-//         "Name": "Yearly launch",
-//         "Deadline": "2022-04-23"
-//       }
-//     }
-//   ], function(err, records) {
-//     if (err) {
-//       console.error(err);
-//       return;
-//     }
-//     records.forEach(function(record) {
-//       console.log(record.get('Name'));
-//     });
-//   });
+app.post('/update', (req,res) => {
+
+    const {recordId, Name, Deadline} = req.body;
+
+    base('Projects').update([
+        {
+          "id": recordId,
+          "fields": {
+            Name,
+            Deadline
+          }
+        }
+      ], function(err, records) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        records.forEach(function(record) {
+          console.log(record.get('Name'));
+        });
+      });
+
+      return res.json({message: 'Update successful'});
+});
+
+//Create records route
+
+app.post('/create', (req,res) => {
+
+    const {Name,Deadline} = req.body;
+
+    base('Projects').create([
+        {
+          "fields": {
+            Name,
+            Deadline
+          }
+        }
+      ], function(err, records) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        return res.json({recordId: records[0].getId()});
+      });
+});
+
+//Delete records route
+
+app.post('/delete', (req,res) => {
+
+    const {recordId} = req.body;
+
+    base('Projects').destroy([recordId],
+        function(err, deletedRecords) {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        console.log('Deleted', deletedRecords.length, 'records');
+        return res.json({message: 'record successfully deleted'});
+    });
+});
 
 
-app.listen(3000, () => console.log('Server listening on port ' + 3000));
+app.listen(3001, () => console.log('Server listening on port ' + 3001));
